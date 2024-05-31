26167c24ba66a633296bd187aaf8f044eb53c58b
Rocard Fonji <fonkengrocard@gmail.com>
2024-05-31 13:11:44 +0100

    (feat: layout): header and footer layouts

---
+++ src/app/shared/ui/header.component.ts
@@ -0,0 +1,25 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-header',
+  standalone: true,
+  template: `
+    <nav class="navbar navbar-light">
+      <div class="container">
+        <a class="navbar-brand" href="/">conduit</a>
+        <ul class="nav navbar-nav pull-xs-right">
+          <li class="nav-item">
+            <a class="nav-link active" href="/">Home</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="/login">Sign in</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="/register">Sign up</a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  `,
+})
+export class HeaderComponent {}
