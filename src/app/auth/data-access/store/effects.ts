e0c9277daf7e8e062d1f283a926eff17436a9344
Rocard Fonji <fonkengrocard@gmail.com>
2024-05-31 18:16:19 +0100

    (feat: register): create persistence to store access token on register success and effect to redirect user

--- src/app/auth/data-access/store/effects.ts
+++ src/app/auth/data-access/store/effects.ts
@@ -1,17 +1,26 @@
 import { Actions, createEffect, ofType } from '@ngrx/effects';
 import { AuthService } from '../auth.service';
 import { inject } from '@angular/core';
-import { catchError, map, of, switchMap } from 'rxjs';
+import { catchError, map, of, switchMap, tap } from 'rxjs';
 import { authActions } from './actions';
 import { HttpErrorResponse } from '@angular/common/http';
+import { PersistanceService } from '../../../shared/data-access/persistance.service';
+import { Router } from '@angular/router';
 
 export const registerEffect = createEffect(
-  (actions$ = inject(Actions), authService = inject(AuthService)) => {
+  (
+    actions$ = inject(Actions),
+    authService = inject(AuthService),
+    persistanceService = inject(PersistanceService),
+  ) => {
     return actions$.pipe(
       ofType(authActions.register),
       switchMap(({ request }) =>
         authService.register(request).pipe(
-          map((currentUser) => authActions.registerSuccess({ currentUser })),
+          map((currentUser) => {
+            persistanceService.set('accessToken', currentUser.token);
+            return authActions.registerSuccess({ currentUser });
+          }),
           catchError((errorResp: HttpErrorResponse) =>
             of(authActions.registerFailure({ errors: errorResp.error.errors })),
           ),
@@ -20,4 +29,16 @@ export const registerEffect = createEffect(
     );
   },
   { functional: true },
+);
+
+export const redirectAfterRegisterEffect = createEffect(
+  (actions$ = inject(Actions), router = inject(Router)) => {
+    return actions$.pipe(
+      ofType(authActions.registerSuccess),
+      tap(() => {
+        router.navigateByUrl('/');
+      }),
+    );
+  },
+  { functional: true, dispatch: false },
 );
