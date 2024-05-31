c9f0a1f6205fc9ceae6535c5ee1f4a2fa8e8f376
Rocard Fonji <fonkengrocard@gmail.com>
2024-05-31 19:57:55 +0100

    (feat: layout): use data from ngrx store to show correct header links

--- src/app/shared/ui/header.component.ts
+++ src/app/shared/ui/header.component.ts
@@ -1,27 +1,56 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { Store } from '@ngrx/store';
+import { selectCurrentUser } from '../../auth/data-access/store/reducers';
+import { AsyncPipe } from '@angular/common';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink, AsyncPipe],
   template: `
     <nav class="navbar navbar-light">
       <div class="container">
-        <a class="navbar-brand" href="/">conduit</a>
+        <a class="navbar-brand" routerLink="/">conduit</a>
         <ul class="nav navbar-nav pull-xs-right">
           <li class="nav-item">
-            <a class="nav-link active" href="/">Home</a>
+            <a class="nav-link active" routerLink="/">Home</a>
+          </li>
+          @if (currentUser$ | async; as currentUser) {
+          <li class="nav-item">
+            <a class="nav-link" routerLink="/articles/new">
+              <i class="ion-compose"></i>&nbsp;New Article
+            </a>
           </li>
           <li class="nav-item">
-            <a class="nav-link" href="/login">Sign in</a>
+            <a class="nav-link" routerLink="/settings">
+              <i class="ion-gear-a"></i>&nbsp;Settings
+            </a>
           </li>
           <li class="nav-item">
-            <a class="nav-link" href="/register">Sign up</a>
+            <a
+              class="nav-link"
+              [routerLink]="['/profiles', currentUser.username]"
+            >
+              <img class="user-pic" [src]="currentUser.image" />
+              &nbsp; {{ currentUser.username }}
+            </a>
           </li>
+          } @else {
+          <li class="nav-item">
+            <a class="nav-link" routerLink="/login">Sign in</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" routerLink="/register">Sign up</a>
+          </li>
+          }
         </ul>
       </div>
     </nav>
   `,
 })
-export class HeaderComponent {}
+export class HeaderComponent {
+  private store = inject(Store);
+
+  currentUser$ = this.store.select(selectCurrentUser);
+}
