c990763b2db0946a198b9ff6662c00ffd720c72f
Rocard Fonji <fonkengrocard@gmail.com>
2024-06-03 15:16:46 +0100

    (feat: article): render article page

--- src/app/articles/article.routes.ts
+++ src/app/articles/article.routes.ts
@@ -1,9 +1,20 @@
 import { Route } from '@angular/router';
 import { ArticleComponent } from './feature/article/article.component';
+import { provideEffects } from '@ngrx/effects';
+import { provideState } from '@ngrx/store';
+import {
+  articleFeatureKey,
+  articleReducer,
+} from './feature/article/data-access/store/reducers';
+import * as articleEffects from './feature/article/data-access/store/effects';
 
 export const routes: Route[] = [
   {
     path: '',
     component: ArticleComponent,
+    providers: [
+      provideEffects(articleEffects),
+      provideState(articleFeatureKey, articleReducer),
+    ],
   },
 ];
--- src/app/articles/feature/article/article.component.ts
+++ src/app/articles/feature/article/article.component.ts
@@ -1,12 +1,62 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
+import { ActivatedRoute, RouterLink } from '@angular/router';
+import { Store } from '@ngrx/store';
+import { articleActions } from './data-access/store/actions';
+import { combineLatest, filter, map } from 'rxjs';
+import {
+  selectArticleData,
+  selectError,
+  selectIsLoading,
+} from './data-access/store/reducers';
+import { AsyncPipe, DatePipe, NgClass } from '@angular/common';
+import { selectCurrentUser } from '../../../auth/data-access/store/reducers';
+import { ErrorMessageComponent } from '../../../shared/ui/error-message.component';
+import { TagListComponent } from '../../../shared/ui/tag-list.component';
+import { LoadingComponent } from '../../../shared/ui/loading.component';
 
 @Component({
   selector: 'app-article',
   standalone: true,
-  imports: [],
+  imports: [
+    AsyncPipe,
+    RouterLink,
+    TagListComponent,
+    ErrorMessageComponent,
+    LoadingComponent,
+    DatePipe,
+    NgClass
+  ],
   templateUrl: './article.component.html',
-  styleUrl: './article.component.scss'
+  styleUrl: './article.component.scss',
 })
-export class ArticleComponent {
+export class ArticleComponent implements OnInit {
+  private store: Store = inject(Store);
+  private route: ActivatedRoute = inject(ActivatedRoute);
 
+  slug = this.route.snapshot.paramMap.get('slug') ?? '';
+
+  isAuthor$ = combineLatest({
+    article: this.store.select(selectArticleData),
+    currentUser: this.store
+      .select(selectCurrentUser)
+      .pipe(filter((currentUser) => currentUser !== undefined)), //check if currentUser is not undefined
+  }).pipe(
+    map(({ article, currentUser }) => {
+      if (!article || !currentUser) {
+        return false;
+      }
+      return article.author.username === currentUser.username;
+    }),
+  );
+
+  data$ = combineLatest({
+    isLoading: this.store.select(selectIsLoading),
+    error: this.store.select(selectError),
+    article: this.store.select(selectArticleData),
+    isAuthor: this.isAuthor$,
+  });
+
+  ngOnInit(): void {
+    this.store.dispatch(articleActions.getArticle({ slug: this.slug }));
+  }
 }
