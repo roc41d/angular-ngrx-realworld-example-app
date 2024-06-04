b6bf52bda57e57d88863868e026e74172d3d3538
Rocard Fonji <fonkengrocard@gmail.com>
2024-06-04 01:25:51 +0100

    (feat: add-to-favorites): add toggle functionality

--- src/app/shared/feature/add-to-favorites/add-to-favorites.component.ts
+++ src/app/shared/feature/add-to-favorites/add-to-favorites.component.ts
@@ -1,5 +1,11 @@
 import { NgClass } from '@angular/common';
-import { Component, Input } from '@angular/core';
+import { Component, Input, inject } from '@angular/core';
+import { Router } from '@angular/router';
+import { Store } from '@ngrx/store';
+import { addToFavoritesActions } from './data-access/store/actions';
+import { selectCurrentUser } from '../../../auth/data-access/store/reducers';
+import { map, take } from 'rxjs';
+import { selectIsSubmitting } from './data-access/store/reducers';
 
 @Component({
   selector: 'app-add-to-favorites',
@@ -7,6 +13,7 @@ import { Component, Input } from '@angular/core';
   imports: [NgClass],
   template: `
     <button
+      (click)="toggleFavorite()"
       class="btn btn-sm"
       [ngClass]="isFavorited ? 'btn-primary' : 'btn-outline-primary'"
     >
@@ -21,8 +28,36 @@ import { Component, Input } from '@angular/core';
   `,
 })
 export class AddToFavoritesComponent {
+  private store: Store = inject(Store);
+  private router: Router = inject(Router);
+
   @Input() showLabel: boolean = false;
   @Input() isFavorited: boolean = false;
   @Input() favoritesCount: number = 0;
   @Input() articleSlug: string = '';
+
+  currentUser$ = this.store.select(selectCurrentUser);
+
+  toggleFavorite(): void {
+    this.currentUser$.pipe(take(1)).subscribe((currentUser) => {
+      if (!currentUser) {
+        this.router.navigate(['/login']);
+      } else {
+        this.store.dispatch(
+          addToFavoritesActions.addToFavorites({
+            isFavorited: this.isFavorited,
+            slug: this.articleSlug,
+          }),
+        );
+
+        if (this.isFavorited) {
+          this.favoritesCount = this.favoritesCount - 1;
+        } else {
+          this.favoritesCount = this.favoritesCount + 1;
+        }
+
+        this.isFavorited = !this.isFavorited;
+      }
+    });
+  }
 }
